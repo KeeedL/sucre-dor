9c43caea328b7fb2600f236a8feb76f4d57fe26e
Bocquet Gauthier <KeeedL@MacBook-Pro-de-Bocquet.local>
2021-03-27 15:58:06 +0100

    push

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
         <div class="row">
           <div class="intro-text">
             <h1>Sucre d'or</h1>
-            <p>Restaurant / Coffee / Pub</p>
+            <p></p>
             <a href="#about" class="btn btn-custom btn-lg page-scroll">Visiter le site</a> </div>
         </div>
       </div>
@@ -72,20 +72,17 @@
   </div>
 </header>
 <!-- About Section -->
-<div id="about">
+<div id="about" class="align background">
   <div class="container">
     <div class="row">
-      <div class="col-xs-12 col-md-6 ">
-        <div class="about-img"><img src="img/about.jpg" class="img-responsive" alt=""></div>
-      </div>
-      <div class="col-xs-12 col-md-6">
+      <div class="col-xs-12 col-md-8 col-md-offset-2">
+        <div class="about-img"><img src="img/gaufrette.png" class="img-responsive" alt=""></div>
         <div class="about-text">
-          <h2>Our Restaurant</h2>
-          <hr>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare diam commodo nibh.</p>
-          <h3>Awarded Chefs</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare.</p>
         </div>
+      </div>
+
       </div>
     </div>
   </div>
